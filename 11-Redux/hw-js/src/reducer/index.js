02de77a0495cb3d5cb5ff6be1bf104cc9d823d62
Giannina <giannisedler@hotmail.com>
2022-09-06 16:27:20 -0300

    hw-js ya anda pte 1

--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -9,7 +9,24 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
+  switch(action.type){
+      case INCREMENTO:
+        return{
+          ...state, contador: state.contador +1
+        };
+      case DECREMENTO:
+        return {
+          ...state, contador: state.contador -1
+        };
+      default:
+        return {...state}
+        // NO OLVIDARSE NUNCA DEL CASO DEFAULT! SIN ESTE LA APP LO ANDA 
+    }
+  }
+
   
-}
+//            dispatch
+// Componentes ------> Action ------> Reducer -------> Nuevo estado 
+
 
 module.exports = contador;
